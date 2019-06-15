b2e6dfe47e22ec0a82fe239561890053e3494c37
Daniele Basile <asterix24@gmail.com>
2019-06-15 10:54:19 +0200

    Some utils.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -ti -v $(pwd):/opt/nodemcu-firmware marcelstoer/nodemcu-build build
---
+++ flash.sh
@@ -0,0 +1,1 @@
+esptool.py --port /dev/tty.usbserial-1410 write_flash -fm dio 0x0 bin/nodemcu_float_master_20190105-1626.bin
